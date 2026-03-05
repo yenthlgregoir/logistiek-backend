95393c2c0dd8b8fce61011ce803222ea48d331d4
Yenth'l Grégoir <yenthl.gregoir@bumaco.be>
2026-03-05 15:59:44 +0100

    user update

--- service/mailService.js
+++ service/mailService.js
@@ -1,14 +1,12 @@
 import { Resend } from "resend";
 
-
-
 export async function sendInviteEmail(email, link) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
 
-const resend = new Resend(process.env.RESEND_API_KEY);
   try {
     await resend.emails.send({
-      from: "onboarding@resend.dev", // test adres
-      to: email,
+      from: "onboarding@resend.dev", // je eigen mailadres of verified domain
+      to: email, // belangrijk! anders gaat de mail nergens heen
       subject: "Account aangemaakt",
       html: `
         <h2>Welkom</h2>
